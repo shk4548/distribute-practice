e64c0863d2d6b39fe8882e2f1e5ef2a055066cc1
shk4548 <shk4548@gmail.com>
2022-04-05 16:36:38 +0900

    first commit

---
+++ emaillist07/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.JsonResult;
+import com.example.demo.repository.EmaillistRepository;
+import com.example.demo.vo.EmaillistVo;
+
+@RestController
+// @CrossOrigin(origins={"http://localhost:9090"}, allowedHeaders="*", allowCredentials="false", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS, RequestMethod.HEAD})
+@CrossOrigin // CORS 오류 방지
+public class ApiController {
+	
+	@Autowired
+	private EmaillistRepository emaillistRepository;
+	
+	@GetMapping("/api")
+	public ResponseEntity<JsonResult> read(@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(emaillistRepository.findAll(keyword)));
+	}
+	
+	@PostMapping("/api")
+	public ResponseEntity<JsonResult> create(@RequestBody EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(vo));
+	}
+}
